b20f2ed51e0407e4d29dd904a7c8a2f03d87af36
unknown <jordangoulet13@gmail.com>
2020-10-19 12:09:54 -0700

    Remove the now ignored directory "src/api/unsplash.js"

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID AlnC2VChh8YAipP_YT6O_QThGlkBcchENEjJkmit0gY",
-  },
-});
